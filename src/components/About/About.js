a473da485eeddf22babac889248a77eb801e44c3
LoraKamenova <cruzeiro@abv.bg>
2024-12-28 15:31:25 +0200

    Markdown files added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,63 @@
-import React from 'react';
-import { Component } from 'react';
+// import React from 'react';
+// import { Component } from 'react';
+// import './About.css';
+
+// class About extends Component {
+
+//     render() {
+//         return (
+//             <section className='main-section'>
+//                 <p>Добре дошли!</p>
+//             </section>
+//         )
+//     }
+// }
+// export default About;
+
+
+// import React from 'react';
+// import { Component } from 'react';
+// import './About.css';
+
+// class About extends Component {
+
+//     render() {
+//         return (
+//             <section className='main-section'>
+//                 <p>Добре дошли!</p>
+//             </section>
+//         )
+//     }
+// }
+// export default About;
+
+
+import React, { Component } from 'react'
+import ReactMarkdown from 'react-markdown'
+import termsFrPath from '../../assets/posts/post.md'
 import './About.css';
 
-class About extends Component {
 
-    render() {
-        return (
-            <section className='main-section'>
-                <p>Добре дошли!</p>
-            </section>
-        )
-    }
+class About extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { terms: null }
+  }
+
+  componentWillMount() {
+    fetch(termsFrPath).then((response) => response.text()).then((text) => {
+      this.setState({ terms: text })
+    })
+  }
+
+  render() {
+    return (
+      <div className="post-section">
+        <ReactMarkdown className="markdown" children={this.state.terms} />
+      </div>
+    )
+  }
 }
-export default About;
+
+export default About
